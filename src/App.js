a6e48d84640e8137bcdd42fdc7d628039bfab1ac
kirubesh <kirubeshsuresh02@gmail.com>
2024-12-30 12:51:20 +0530

    Initial commit with project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"; // optional for custom styling
+import CalendarComponent from "./CalenderComponet.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My FullCalendar in React</h1>
+      <CalendarComponent />
+    </div>
+  );
+}
+
+export default App;
